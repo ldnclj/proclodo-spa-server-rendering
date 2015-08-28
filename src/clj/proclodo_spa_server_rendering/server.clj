d9bcd28c2d527cdb95bdeb78d9696bf27d205568
J. Pablo Fernández <pupeno@pupeno.com>
2015-08-28 16:26:23 +0100

    Allow specifying the min and max threads.

--- src/clj/proclodo_spa_server_rendering/server.clj
+++ src/clj/proclodo_spa_server_rendering/server.clj
@@ -5,5 +5,10 @@
   (:gen-class))
 
 (defn -main [& args]
-  (let [port (Integer/parseInt (or (env :port) "3000"))]
-    (run-jetty app {:port port :join? false})))
+  (let [config {:port        (Integer/parseInt (or (env :port) "3000"))
+                :join?       false
+                :min-threads (when (env :min-threads) (Integer/parseInt (env :min-threads)))
+                :max-threads (when (env :max-threads) (Integer/parseInt (env :max-threads)))}
+        config (reduce-kv #(if (not (nil? %3)) (assoc %1 %2 %3) %1) {} config)]
+    (println "Running with config:" config)
+    (run-jetty app config)))
